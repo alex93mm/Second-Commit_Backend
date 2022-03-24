f25fad6ca566d6817e9be3ce1d241d2895796511
Alejandro Martínez Martínez <alejandro93mm@gmail.com>
2022-03-24 12:49:37 +0100

    Routing API

---
+++ start/routes.ts
@@ -0,0 +1,24 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.post('auth/register', 'AuthController.register')
+  Route.post('auth/login', 'AuthController.login')
+
+  Route.group(() => {
+    Route.resource('cursos', 'CoursesController').apiOnly().middleware({
+      store: 'admin',
+    })
+
+    Route.resource('temas', 'ThemesController').apiOnly().middleware({
+      store: 'admin',
+      update: 'admin',
+      destroy: 'admin',
+    })
+
+    Route.resource('preguntas', 'DiscussesController').apiOnly()
+    Route.resource('preguntas.votos', 'VotesDiscussesController').apiOnly()
+
+    Route.resource('respuestas', 'ResponsesController').apiOnly()
+    Route.resource('respuestas.votos', 'VotesResponsesController').apiOnly()
+  }).middleware('auth')
+}).prefix('openAPI/foro')
